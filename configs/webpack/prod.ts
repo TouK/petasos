2dbfec8e5504257002a50474584896aaeee91048
Julian Wielga <j@touk.pl>
2021-03-08 13:14:53 +0100

    deps

--- configs/webpack/prod.ts
+++ configs/webpack/prod.ts
@@ -1,22 +1,14 @@
-import DynamicCdnWebpackPlugin from 'dynamic-cdn-webpack-plugin';
 import { resolve } from 'path';
 import merge from 'webpack-merge';
 import { commonConfig } from './common';
 
 export const prodConfig = merge(commonConfig, {
-  mode: 'production',
-  output: {
-    filename: 'js/bundle.min.js',
-    path: resolve(__dirname, '../../dist'),
-  },
-  devtool: 'source-map',
-  plugins: [
-    new DynamicCdnWebpackPlugin({
-      exclude: [
-        'react-router-dom',
-      ],
-    }),
-  ],
+    mode: 'production',
+    output: {
+        filename: 'js/bundle.min.js',
+        path: resolve(__dirname, '../../dist'),
+    },
+    devtool: 'source-map',
 });
 
 export default prodConfig;

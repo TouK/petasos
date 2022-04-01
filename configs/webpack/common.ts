4d32d2c55423d0da32c347ccb876347b5926031c
Julian Wielga <j@touk.pl>
2022-04-01 17:37:13 +0200

    reformat config

--- configs/webpack/common.ts
+++ configs/webpack/common.ts
@@ -2,105 +2,94 @@ import AddAssetHtmlPlugin from "add-asset-html-webpack-plugin";
 import dotenv from "dotenv";
 import ForkTsCheckerWebpackPlugin from "fork-ts-checker-webpack-plugin";
 import HtmlWebpackPlugin from "html-webpack-plugin";
-import {resolve} from "path";
-import {Configuration} from "webpack";
+import { resolve } from "path";
+import { Configuration } from "webpack";
 import pkg from "../../package.json";
 
 dotenv.config();
 
-const findEnvModule = ({request}, callback) => {
-    if (/\/_env$/.test(request)) {
-        return callback(null, "window['.env']");
-    }
-    callback(undefined, undefined);
+const findEnvModule = ({ request }, callback) => {
+  if (/\/_env$/.test(request)) {
+    return callback(null, "window['.env']");
+  }
+  callback(undefined, undefined);
 };
 
 export const commonConfig: Configuration = {
-    resolve: {
-        extensions: [
-            ".tsx",
-            ".ts",
-            ".jsx",
-            ".js"
+  resolve: {
+    extensions: [".tsx", ".ts", ".jsx", ".js"],
+    symlinks: false,
+  },
+  context: resolve(__dirname, "../.."),
+  target: "web",
+  output: {
+    publicPath: "",
+  },
+  externals: [findEnvModule],
+  module: {
+    rules: [
+      {
+        test: /\.[jt]sx?$/,
+        exclude: /node_modules/,
+        use: [{ loader: "babel-loader" }],
+      },
+      {
+        test: /\.css$/,
+        use: [
+          { loader: "style-loader" },
+          {
+            loader: "css-loader",
+            options: {
+              importLoaders: 1,
+              modules: {
+                mode: "local",
+                exportLocalsConvention: "dashes",
+              },
+            },
+          },
         ],
-        symlinks: false
-    },
-    context: resolve(__dirname, "../.."),
-    target: "web",
-    output: {
-        publicPath: ""
-    },
-    externals: [
-        findEnvModule
-    ],
-    module: {
-        rules: [
-            {
-                test: /\.[jt]sx?$/,
-                exclude: /node_modules/,
-                use: [
-                    {loader: "babel-loader"}
-                ]
+      },
+      {
+        test: /\.(jpe?g|png|gif|svg)$/i,
+        use: [
+          {
+            loader: "file-loader",
+            options: {
+              name: "img/[hash].[ext]",
             },
-            {
-                test: /\.css$/,
-                use: [
-                    {loader: "style-loader"},
-                    {
-                        loader: "css-loader",
-                        options: {
-                            importLoaders: 1,
-                            modules: {
-                                mode: "local",
-                                exportLocalsConvention: "dashes"
-                            }
-                        }
-                    }
-                ]
+          },
+          {
+            loader: "image-webpack-loader",
+            options: {
+              bypassOnDebug: true,
+              optipng: {
+                optimizationLevel: 7,
+              },
+              gifsicle: {
+                interlaced: false,
+              },
             },
-            {
-                test: /\.(jpe?g|png|gif|svg)$/i,
-                use: [
-                    {
-                        loader: "file-loader",
-                        options: {
-                            name: "img/[hash].[ext]"
-                        }
-                    },
-                    {
-                        loader: "image-webpack-loader",
-                        options: {
-                            bypassOnDebug: true,
-                            optipng: {
-                                optimizationLevel: 7
-                            },
-                            gifsicle: {
-                                interlaced: false
-                            }
-                        }
-                    }
-                ]
-            }
-        ]
-    },
-    entry: [
-        "./src/index.tsx"
+          },
+        ],
+      },
     ],
-    plugins: [
-        new ForkTsCheckerWebpackPlugin(),
-        new HtmlWebpackPlugin({
-            title: `${pkg.name} ${pkg.version}`
-        }),
-        new AddAssetHtmlPlugin({
-            filepath: resolve(__dirname, "../../_env.js")
-        })
-    ],
-    performance: {
-        hints: false
-    },
-    optimization: {
-        runtimeChunk: false,
-        mergeDuplicateChunks: true,
-        concatenateModules: true
-    }
+  },
+  entry: ["./src/index.tsx"],
+  plugins: [
+    new ForkTsCheckerWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      title: `${pkg.name} ${pkg.version}`,
+    }),
+    new AddAssetHtmlPlugin({
+      filepath: resolve(__dirname, "../../_env.js"),
+    }),
+  ],
+  performance: {
+    hints: false,
+  },
+  optimization: {
+    runtimeChunk: false,
+    mergeDuplicateChunks: true,
+    concatenateModules: true,
+  },
 };
--- configs/webpack/prod.ts
+++ configs/webpack/prod.ts
@@ -1,14 +1,14 @@
-import { resolve } from 'path';
-import merge from 'webpack-merge';
-import { commonConfig } from './common';
+import { resolve } from "path";
+import merge from "webpack-merge";
+import { commonConfig } from "./common";
 
 export const prodConfig = merge(commonConfig, {
-    mode: 'production',
-    output: {
-        filename: 'js/bundle.min.js',
-        path: resolve(__dirname, '../../dist'),
-    },
-    devtool: 'source-map',
+  mode: "production",
+  output: {
+    filename: "js/bundle.min.js",
+    path: resolve(__dirname, "../../dist"),
+  },
+  devtool: "source-map",
 });
 
 export default prodConfig;

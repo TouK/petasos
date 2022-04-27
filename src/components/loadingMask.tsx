24b7ca3e077e316f31116ddf079eeed679472da2
Julian Wielga <j@touk.pl>
2022-04-27 10:32:20 +0200

    feat: cleaner loading indicator

--- src/components/loadingMask.tsx
+++ src/components/loadingMask.tsx
@@ -1,17 +1,13 @@
-import { Backdrop, CircularProgress } from "@mui/material";
+import { Backdrop, Box, LinearProgress } from "@mui/material";
 import React from "react";
 import { createPortal } from "react-dom";
 
 export function LoadingMask({ open }: { open: boolean }) {
   return createPortal(
-    <Backdrop
-      open={open}
-      style={{
-        color: "white",
-        zIndex: 1,
-      }}
-    >
-      <CircularProgress color="inherit" />
+    <Backdrop invisible open={open}>
+      <Box flex={1} alignSelf="flex-start">
+        <LinearProgress color="secondary" />
+      </Box>
     </Backdrop>,
     document.body
   );
